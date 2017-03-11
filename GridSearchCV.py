ab54c3c7ad8944d07db5ec527b74c89b69b1ac79
piokuc <peter@kuchta.co.uk>
2017-03-11 16:49:31 +0000

    Clean up

--- GridSearchCV.py
+++ GridSearchCV.py
@@ -8,7 +8,7 @@ from sklearn.model_selection import GridSearchCV
 # Id,region,date,mortality_rate,O3,PM10,PM25,NO2,T2M
 
 # Test set: as above, except it does not have mortality_rate
-features = ['O3','PM10','PM25','NO2','T2M'] # ignore region & date and for now
+features = ['O3','PM10','PM25','NO2','T2M'] # ignore region & date for now
 
 data_dir = '.'
 
@@ -25,14 +25,11 @@ y = train['mortality_rate'].copy()
 
 X_test = test[features].copy()
 
-"""
-lr = LinearRegression()
-lr.fit(X_train, y)
-"""
 parameters = {'kernel':('linear', 'rbf'), 'C':[1, 2, 5, 8, 10]}
 svr = svm.SVR()
-""" http://scikit-learn.org/stable/modules/generated/sklearn.model_selection.GridSearchCV.html
-"""
+
+# http://scikit-learn.org/stable/modules/generated/sklearn.model_selection.GridSearchCV.html
+
 model = GridSearchCV(svr, parameters, n_jobs = 4, verbose = 99)
 print "best model:", model
 print dir(model)
